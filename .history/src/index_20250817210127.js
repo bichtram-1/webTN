9f38d921f9d1a94ff1dc728edfac28ffff409e94
Bich Tram <0306231166@caothang.edu.vn>
2025-08-17 21:22:13 +0700

    video_48.2

--- .history/src/index_20250817210127.js
+++ .history/src/index_20250817210127.js
@@ -4,8 +4,6 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
 import {
   BrowserRouter,
   Routes,
